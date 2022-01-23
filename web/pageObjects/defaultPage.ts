2e40393e728f6003e5a2bd8b8afdc4f85002e7af
Jurgita Sciupakoviene <jurgita.sciupakoviene@telesoftas.com>
2022-01-23 22:12:36 +0200

    Updated defaultPage, upgraded landingPage test and started updating loginWithEmail test

--- web/pageObjects/defaultPage.ts
+++ web/pageObjects/defaultPage.ts
@@ -1,11 +1,39 @@
+//GET ELEMENTS
+
 export async function getElementByLocator(locator: string) {
     return await (browser).$(locator)
 }
+
+export async function getElementTextByLocator(locator:string):Promise<string>{
+    return (await getElementByLocator(locator)).getText()
+}
+
+//ACTIONS
 
 export async function clickByLocator(locator: string): Promise<void> {
     await (await getElementByLocator(locator)).click()
 }
 
+export async function typeTextByLocator (text:string | string[],locator:string):Promise<void>{
+    await (await getElementByLocator(locator)).keys(text)
+}
+
+export async function appendTextByLocator(text: string, locator: string): Promise<void> {
+    await (await getElementByLocator(locator)).addValue(text)
+}
 export async function openLandingPage(): Promise<void> {
     await browser.url('')
 } 
+//OTHER
+let defaultTimeout: number = 2000
+
+export async function waitUntilElementIsVisibleInViewportByLocator(locator: string, customTimeout?: number,): Promise<void> {
+    const timeoutMessage = `${locator} element still invisible after ${customTimeout || defaultTimeout} ms`
+    await browser.waitUntil(async function() {
+          return (await getElementByLocator(locator)).isDisplayedInViewport()
+      },
+      {
+          timeout: customTimeout || defaultTimeout,
+          timeoutMsg: timeoutMessage
+      })
+    }
--- web/tests/landingPage.ts
+++ web/tests/landingPage.ts
@@ -2,9 +2,7 @@ import * as loginPage from "../pageObjects/login.page"
 import * as defaultPage from "../pageObjects/defaultPage"
 import {expect} from 'chai'
 
-const email ='Tekstas'
-
-describe('Test for login with email', function () {
+describe('Test for landing page funcionality', function () {
     this.timeout(0)
 
     it('Clicks sign in with email button', async function () {
@@ -12,13 +10,8 @@ describe('Test for login with email', function () {
        await browser.maximizeWindow();
        await loginPage.clickSignInWithEmailButton();
        await loginPage.waitForSignInForm()
-       expect(await loginPage.getSignInHeaderText()).equals("Sign in")
-       await browser.pause(10000);
-       await loginPage.typeEmail(email);
-       await browser.pause(10000);
-
+       expect(await loginPage.getSignInHeaderText()).equals("Sign in");
+       await browser.closeWindow()
     })
 
 }) 
-
-
