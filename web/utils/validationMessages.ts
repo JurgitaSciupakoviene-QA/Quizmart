d04dfeef728536bc93b6e283bb0b6ac0556e8a55
Jurgita Sciupakoviene <jurgita.sciupakoviene@telesoftas.com>
2022-01-28 15:30:24 +0200

    Error messages were added in the Utils

---
+++ web/utils/validationMessages.ts
@@ -0,0 +1,12 @@
+export enum validationMessages {
+   emailRequired ='Required',
+   passRequired = 'Required',
+   passLengthError = 'Password must be at least of 6 characters in length',
+   tooLongPassError = 'Password must be at most of 100 characters in length',
+   incorrectEmailAndPassError = 'The email address or password is incorrect',
+   incorrectEmailError = 'Must be valid email'
+
+
+
+
+}
